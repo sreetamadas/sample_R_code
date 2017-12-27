f6f29062812fcbad0b78775ec823a56af0047b4a
sreetamadas <sreetama.d2005@gmail.com>
2017-12-27 20:34:29 +0530

    Update R_plots.r

--- R_plots.r
+++ R_plots.r
@@ -64,12 +64,9 @@ cor(new)  # check correlation among the variables
 plot(df$x, df$y, col=rgb(0,100,0,50,maxColorValue=255), pch=16, xlab="X", ylab="Y") 
 
 # method 2 : (alpha sets the transparency)
-Palette1 <- c('red','blue','violet','black')  # the 4 colours are for the 4 levels of 'fac' in the df
 library(ggplot2)
 ggplot(data=df, aes(df$ab, df$bc, color=fac)) + geom_point(alpha=0.3) +
-    labs(x="var A-B", y="var B-C") + scale_colour_manual(values=Palette1) + 
-    scale_colour_discrete(drop=TRUE, limits = levels(fac)) # + geom_line(size=0.01, col="blue")
-  
+    labs(x="var A-B", y="var B-C")   
 
 
 ### 3D scatterplots ###
