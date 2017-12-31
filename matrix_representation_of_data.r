21fffbea2396724db0d81a3d31a3a365d4a34ee5
sreetamadas <sreetama.d2005@gmail.com>
2017-12-31 12:45:22 +0530

    Update matrix_representation_of_data.r

--- matrix_representation_of_data.r
+++ matrix_representation_of_data.r
@@ -1,4 +1,4 @@
-## method 2 : plot X per ID, per month; as a matrix / heat map
+## plot X per ID, per month; as a matrix / heat map
 
 ## read data
 
@@ -32,4 +32,49 @@ corrplot(t(production),is.corr = FALSE, method='square')
 library(RColorBrewer)
 corrplot(t(production),is.corr = FALSE, method='color', col=brewer.pal(n=9, name='Blues'))  
 ## the top 25 are highlighted in the matrix
+
+###########################################################################################
+### plotting daily data for a month as matrix
+
+# find month, week and day of week for each entry
+### day of week
+mc$day <- as.POSIXlt(mc$Date)$wday
+## week number
+mc$week <- format(as.POSIXct(mc$Date, format='%Y-%m-%d'), format="%U")
+## month
+mc$month <- format(as.POSIXct(mc$Date, format='%Y-%m-%d'), format="%m")
+
+## extract data for a selected month
+selected_month <- 10
+
+temp <- subset(mc, mc$month == selected_month)
+
+# create list of week number & day-of-week in selected data
+dowList <-  sort(unique(temp$day)) ## get unique IDs in machine column
+weekList <- as.integer(sort(unique(temp$week)))
+
+# generate matrix of data for the month
+new_dat <- matrix(nrow=length(weekList), ncol=length(dowList))
+
+for(i in 1:length(weekList) ) {
+  for(j in 1:length(dowList) ) {
+    t <- subset(temp, temp$week == weekList[i] & temp$day == dowList[j])
+    
+    if(nrow(t) == 0) {
+      new_dat[i,j] <- -1
+    }
+    else {
+      new_dat[i,j] <- t$Y
+    }
+  }
+}
+
+
+# plot as matrix
+library(corrplot)
+corrplot(energy,is.corr = FALSE, method='square')
+
+
+
+
 
