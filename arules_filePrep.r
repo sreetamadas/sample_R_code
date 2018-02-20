61125b5656f06176161b5ada6692ab177a895fba
sreetamadas <sreetama.d2005@gmail.com>
2018-02-20 13:59:36 +0530

    Update arules_filePrep.r

--- arules_filePrep.r
+++ arules_filePrep.r
@@ -7,20 +7,37 @@
 # and we collapse the resulting colnames into one string
 # then bind the previous data.frame (only the dateTime column actually) with the new values into a column called D
 
+# rename cols
+names(df)[names(df) == "D4"] <- "dev1"
+names(df)[names(df) == "D5"] <- "AC2"
+
+# create combined column
+df$combi_set <- ifelse(df$D3==1 | df$D8==1 | df$D9==1 | df$D10==1 | df$D11==1 | df$D12==1 | df$D13==1 | df$D14==1 | df$D15==1, 1, 0)
+
 # remove unnecessary columns
 df$D1 <- NULL
 df$day <- NULL
+df <- df[ , !names(df) %in% c("A","B","C","D1","D2","D3","D8","D9","D10","D11","D12",
+                                  "D13","D14","D15","D17")]   # "time",
 
-# format data appropriately
-cn <- colnames(df)[-(1:5)]
+
+# format data appropriately (create a single col with ON-states of different cols)
+df <- df[c("time","id","track", "dev1", "AC2","combi_set")]  # reorder by column name
+cn <- colnames(df)[-(1:3)]   # save names of col except 1st five 
 new_df <- cbind(df['time'], #df['id'],df['A'],df['B'],df['C'],
-                device=apply( df[,-(1:5)] != 0, 1 , function(x) paste(cn[x],collapse=',')))
+                device=apply( df[,-(1:3)] != 0, 1 , function(x) paste(cn[x],collapse=',')))
 
-## remove timeDate, & empty rows in device col
+
+## method1:  remove timeDate, & empty rows in device col
 new_df$time <- NULL
 new_df <- data.frame(new_df[!(new_df$device == "") ,])
 colnames(new_df)[1] <- 'device'
 
+## method 2: alternately, fill cells containing NA with pre-decided string
+new_df$device[new_df$device == ""] <- NA
+new_df$device <- as.character(new_df$device)
+new_df$device[is.na(new_df$device)] <- "steady"
+                             
                              
                              
 ## reshaping data: from long to wide format                             
