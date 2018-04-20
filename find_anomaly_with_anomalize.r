23c46d924895a22d1f3f5e45fc94b0f147c63fd5
sreetamadas <sreetama.d2005@gmail.com>
2018-04-20 19:59:42 +0530

    Update find_anomaly_with_anomalize.r

--- find_anomaly_with_anomalize.r
+++ find_anomaly_with_anomalize.r
@@ -6,10 +6,19 @@ library(anomalize)
 
 # https://business-science.github.io/anomalize/
 T %>%
+   # decompose into trend, seasonality & remainder
   time_decompose(temp) %>% #, method = "stl", frequency = "auto", trend = "auto", message = TRUE) %>%    ## method = stl / twitter
-  anomalize(remainder, method = "iqr") %>%    # method = iqr or gesd
+
+  # anomaly detection on the remainder component using IQR
+  anomalize(remainder, method = "iqr") %>%    # method = iqr or gesd  ; default: alpha = 0.05, max_anoms = 0.2
+
+  # create the lower and upper bounds around the “observed” values
   time_recompose() %>%
+
+  # plot
   plot_anomalies(time_recomposed = TRUE, ncol = 3, alpha_dots = 0.5)
+
+  # visualize anomalies superposed on the STL decomposition
   #plot_anomaly_decomposition() +
   #labs(title = "Decomposition of Anomalized temp.")
 
