522bd1f498bc1f73df82edc749a776012f5d58b8
sreetamadas <sreetama.d2005@gmail.com>
2018-05-29 09:24:35 +0530

    Update and rename select_attributes_random_forest.r to feature_selection_random_forest.r

--- feature_selection_random_forest.r
+++ feature_selection_random_forest.r
@@ -2,6 +2,27 @@
 
 ## read in data
 
+###########################################################################################
+##### correlation among different variables #####
+t <- df[,c(5,9,14:ncol(df))]
+#t <- t[,apply(t, 2, var, na.rm=TRUE) != 0]  # remove columns with constant, variance = 0
+library(corrplot)
+#par(mar = c(10,4,4,4)) # space on the bottom, left, top, and right.
+corrplot(cor(d[,c(5,9,14:ncol(d))]),type="lower", method="color", tl.cex=0.6, cl.cex=0.8)
+
+
+#### PCA  #####
+t <- df[,c(5,14:ncol(df))]     # take numeric columns only, not factors
+#t <- t[, colSums(t != 0) > 0]    # remove columns with only zeroes
+t <- t[,apply(t, 2, var, na.rm=TRUE) != 0]  # remove columns with constant, variance = 0
+#t <- t[,sapply(t, function(v) var(v, na.rm=TRUE)!=0)]
+#str(t)  # shows types of data in different columns
+m.pca <- prcomp(t, center = TRUE, scale. = TRUE)   # take PCA
+#print(m.pca)
+plot(m.pca, type='l')
+plot(m.pca$x[,1:2],)
+summary(m.pca)
+##########################################################################################
 
 ####  to find features using classification ####
 # step 1: convert continuous data to categorical data (class labels) using binning
