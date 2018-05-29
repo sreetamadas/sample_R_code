ddf58af9d492a09f1524f223b3eff8439864de5a
sreetamadas <sreetama.d2005@gmail.com>
2018-05-29 17:06:30 +0530

    Update test_code.r

--- test_code.r
+++ test_code.r
@@ -215,8 +215,10 @@ new_df <- data.frame(as.character(levels(as.factor(df$ID))))
 colnames(new_df)[1] <- 'ID'
 total <- tapply(df$TotalPcs, df$ID, FUN=sum)
 sd_T <- tapply(df$TotalPcs, df$ID, FUN=sd)  # sample FUN : sum, mean, median, sd
+             
 num_of_instances <- tapply(df$someVariable, df$ID, function(x) length(unique(x))) # count no. of occurrences of each ID
 ## use length(unique(x))  or length(x) according to need/ context
+                           
 new_df <- cbind(new_df, total, sd_T)
 ## remove NA values
 new_df <- new_df[complete.cases(new_df),]    
